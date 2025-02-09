662201c4c4dbb4a2ca22badd90affd89a25533f4
luke <luke@gmail.com>
2025-02-09 04:26:08 -0800

    create model gallery_image main app

--- main/models.py
+++ main/models.py
@@ -104,3 +104,34 @@ class MembershipPlan(models.Model):
 
     def __str__(self):
         return self.name       
+
+
+
+
+
+from django.db import models
+
+class Gallerys(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='gallery/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    event_date = models.DateField()  # Removed extra space
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
