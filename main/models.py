24039ecae4998530cd8d3c61963a7c8abb7a1957
simon <simonnyegenye@gmail.com>
2025-04-07 03:47:29 -0700

    created the model and added the data on the main app

--- main/models.py
+++ main/models.py
@@ -180,7 +180,14 @@ class News(models.Model):
 
 
 
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+    submitted_at = models.DateTimeField(auto_now_add=True)  # Automatically sets the timestamp when created
 
+    def __str__(self):
+        return f"Message from {self.name} - {self.email}"
 
 
 
