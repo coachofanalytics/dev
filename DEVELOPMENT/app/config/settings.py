6da5f2f31213b94f625f56b755b66b3c0916b039
masakhwe <markmanu21@gmail.com>
2025-04-24 23:29:53 +0300

    Transferred Database credentials to settings.py file

--- DEVELOPMENT/app/config/settings.py
+++ DEVELOPMENT/app/config/settings.py
@@ -1,52 +1,27 @@
-
+import os
 #  ==============DBFUNCTIONS=====================================
 def dba_values():
     if os.environ.get('ENVIRONMENT') == 'production':
-        host = os.environ.get('HEROKU_DYCPROD_HOST')
-        dbname = os.environ.get('HEROKU_DYCPROD_NAME')
-        user = os.environ.get('HEROKU_DYCPROD_USER')
-        password = os.environ.get('HEROKU_DYCPROD_PASS')
+        host = os.environ.get('PROD_FASTAPI_DB_HOST')
+        dbname = os.environ.get('PROD_FASTAPI_DB_NAME')
+        user = os.environ.get('PROD_FASTAPI_DB_USER')
+        password = os.environ.get('PROD_FASTAPI_DB_PASS')
+        port = 5432
 
     elif os.environ.get('ENVIRONMENT') == 'staging':
-        # In Heroku/Postgres it is Heroku_UAT
-        host = os.environ.get('DB_NAME')
-        dbname = os.environ.get('DB_NAME')
-        user = os.environ.get('DB_USER')
-        password = os.environ.get('DB_PASSWORD')
+        host = os.environ.get('STG_FASTAPI_DB_HOST')
+        dbname = os.environ.get('STG_FASTAPI_DB_NAME') 
+        user = os.environ.get('STG_FASTAPI_DB_USER')
+        password = os.environ.get('STG_FASTAPI_DB_PASSWORD')
+        port = 5432
 
     else:
-        #Test in staging database before staging deployment
-        host = os.environ.get('STG_DB_HOST')
-        dbname = os.environ.get('STG_DB_NAME') 
-        user = os.environ.get('STG_DB_USER')
-        password = os.environ.get('STG_DB_PASSWORD')
+        #Test locally 
+        host = os.environ.get('LOCAL_FASTAPI_DB_HOST')
+        dbname = os.environ.get('LOCAL_FASTAPI_DB_NAME') 
+        user = os.environ.get('LOCAL_FASTAPI_DB_USER')
+        password = os.environ.get('LOCAL_FASTAPI_DB_PASSWORD') 
+        port = 5432
 
-        # #Test locally 
-        # host = os.environ.get('LOCAL_DB_HOST')
-        # dbname = os.environ.get('LOCAL_DB_NAME') 
-        # user = os.environ.get('LOCAL_DB_USER')
-        # password = os.environ.get('LOCAL_DB_PASSWORD') 
 
-        host = 'localhost'
-        dbname = 'DC48K_DEV'
-        user = 'postgres'
-        password = 'postgres' 
-
-    return host,dbname,user,password  
-
-WSGI_APPLICATION = "coda_project.wsgi.application"
-import dj_database_url
-
-host,dbname,user,password=dba_values() #herokuprod() #herokudev() #dblocal()  #herokudev(),
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": dbname,
-        "USER":user,
-        "PASSWORD":password,
-        "HOST": host
-    }
-}
+    return host,dbname,user,password,port 
--- DEVELOPMENT/app/core/db/database.py
+++ DEVELOPMENT/app/core/db/database.py
@@ -1,9 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config.settings import dba_values
+
+host,dbname,user,password,port = dba_values()
 
 # Database URL: Format for PostgreSQL: 'postgresql://username:password@localhost:5432/dbname'
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/FASTAPI"
+# DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/FASTAPI"
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
 
 # Create an engine for postgreSQL database connection
 engine = create_engine(DATABASE_URL)
