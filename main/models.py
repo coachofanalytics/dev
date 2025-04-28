824e2de07ff8b0b81613df7505ab776db386c7b1
Alvin_Shedrack <alvinshedrack90@gmail.com>
2025-04-28 18:49:40 +0300

    create model and admin in main app

--- main/models.py
+++ main/models.py
@@ -168,3 +168,15 @@ class ContactMessage2(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} - {self.email}"
+
+
+
+class Donation(models.Model):
+    donor_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.donor_name} - {self.amount}"
