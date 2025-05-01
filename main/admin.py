c8a16b66940882f0ec755b9d466e5beafb66029f
Your Name <Brenda>
2025-05-01 10:34:08 +0300

    create a modle and admin  main app

--- main/admin.py
+++ main/admin.py
@@ -23,6 +23,6 @@ admin.site.register(Description, DescriptionAdmin)
 #admin.site.register(ContactUs)
 #admin.site.register(Faq)
 #admin.site.register(Volunteer)
-
+#admin.site.register(Donation)
 
 
--- main/models.py
+++ main/models.py
@@ -201,6 +201,24 @@ class Volunteer(models.Model):
 
 
 
+class Donation(models.Model):
+    donor_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.donor_name} - {self.amount}"
+
+
+
+
+
+
+
+
+
 
 
 
