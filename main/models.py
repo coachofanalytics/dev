cb5d1718657ec0ce659047504ae988ce5485244a
Cecelia <djinn676@gmail.com>
2025-06-05 13:43:43 -0500

    added string representation of the governance model

--- main/models.py
+++ main/models.py
@@ -202,3 +202,5 @@ class Governance(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)  
     updated_at = models.DateTimeField(auto_now=True) 
 
+    def __str__(self):
+        return self.structure_category
