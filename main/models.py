c0889c5ea599d87a5447dbbb7e2d943a6bdfad32
Cecelia <djinn676@gmail.com>
2025-06-05 14:04:42 -0500

    removed structure from model

--- main/models.py
+++ main/models.py
@@ -202,5 +202,3 @@ class Governance(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)  
     updated_at = models.DateTimeField(auto_now=True) 
 
-    def __str__(self):
-        return self.governance_category
