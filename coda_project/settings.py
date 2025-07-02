c0cc9a36e6fc0cb527acbcb5bec13f6ea6c5ba17
coachofanalytics <coachofanalytics@gmail.com>
2025-07-03 00:12:57 +0200

    clone install 25.06_DC48K_STG_BL Locally

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+print(SECRET_KEY)
 
 #DEBUG Configurations
 DEBUG = True
@@ -136,16 +136,16 @@ def dba_values():
 
     else:
         # Test in staging database before staging deployment
-        # host = os.environ.get('STG_DB_HOST')
-        # dbname = os.environ.get('STG_DB_NAME') 
-        # user = os.environ.get('STG_DB_USER')
-        # password = os.environ.get('STG_DB_PASSWORD')
+        host = os.environ.get('STG_DB_HOST')
+        dbname = os.environ.get('STG_DB_NAME') 
+        user = os.environ.get('STG_DB_USER')
+        password = os.environ.get('STG_DB_PASSWORD')
 
         #Test locally 
-        host = os.environ.get('LOCAL_DB_HOST')
-        dbname = os.environ.get('LOCAL_DB_NAME') 
-        user = os.environ.get('LOCAL_DB_USER')
-        password = os.environ.get('LOCAL_DB_PASSWORD') 
+        # host = os.environ.get('LOCAL_DB_HOST')
+        # dbname = os.environ.get('LOCAL_DB_NAME') 
+        # user = os.environ.get('LOCAL_DB_USER')
+        # password = os.environ.get('LOCAL_DB_PASSWORD') 
 
 
 
@@ -157,7 +157,7 @@ import dj_database_url
 host,dbname,user,password=dba_values() #herokuprod() #herokudev() #dblocal()  #herokudev(),
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+print(host,dbname,user,password)
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
