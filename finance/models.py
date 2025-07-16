a63277313ccd1cc634617eefe13b3c5772ce7008
Cecelia <djinn676@gmail.com>
2025-07-16 18:57:48 -0500

    created a flow to save customer and transaction infornmation from paypal to customeruser model and payment model respectively in finance app

--- finance/models.py
+++ finance/models.py
@@ -378,7 +378,7 @@ class Payment(models.Model):
         ("donation", "Donation"),
     ]
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    payment_type = models.CharField(max_length=20, choices=PAYMENT_TYPE_CHOICES)
+    payment_type = models.CharField(max_length=100, choices=PAYMENT_TYPE_CHOICES)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_id = models.CharField(max_length=100, unique=True)
     status = models.CharField(
