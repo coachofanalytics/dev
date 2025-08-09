486dc58a4bfe9dba0de0183aa5d0c28c688b3bdf
masakhwe <markmanu21@gmail.com>
2025-08-09 03:15:33 +0300

    Live stripe setup and resolved issue with the cancel url

--- finance/urls.py
+++ finance/urls.py
@@ -87,7 +87,7 @@ urlpatterns = [
 
     path("stripe/checkout/", views.stripe_checkout, name="stripe_checkout"),
     path("stripe/success/", views.stripe_payment_success, name="stripe_success"),
-    path("stripe/cancel/", views.stripe_payment_success, name="stripe_cancel"),
+    path("stripe/cancel/", views.stripe_payment_cancel, name="stripe_cancel"),
     path("stripe/webhook/", views.stripe_webhook, name="stripe_webhook"),
 
 ]
