16a58ac192fd49dce0fb11d399a3f70821e9b9c6
Chris Maghas <coachofanalytics@gmail.com>
2025-09-15 10:23:14 -0500

    changing  chromedriver-py==121.0.6167.85

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -147,7 +147,12 @@ DATABASES = {
     
     }
 }
-
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'coda_dev'
+    }
 
 
 '''=========== Heroku DB ================'''
