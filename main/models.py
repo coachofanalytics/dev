913740d6ee2cfd14e712d1ceb90059631d60f641
Ihirwe Ndegeya Fadhiri <ndegeyafadhiri@gmail.com>
2025-09-22 23:26:36 +0200

    fixed  donation_organization model issue

--- main/models.py
+++ main/models.py
@@ -160,5 +160,14 @@ class ContactUs(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} ({self.email})"
-
     
+class Donation_organization(models.Model):
+    donor_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.donor_name} - {self.amount}"
+
