2b29970d5b4f8c26a17f88fbd3a44c82615c8dff
Luke <info@2009_coda>
2025-10-01 22:52:32 +0300

    code

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import CustomerUser,Department,Credential,TaskGroup,TeamMember
+from .models import CustomerUser,Department,Credential,TaskGroup,TeamMembers
 
 
 
