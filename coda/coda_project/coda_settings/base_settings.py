8de475234703d803252caa0206217d6ed0f7a360
CODA Deploy <deploy@codamakutano.com>
2025-10-30 00:48:01 -0500

    chore(settings): register platform_services in INSTALLED_APPS to expose management commands

--- coda/coda_project/coda_settings/base_settings.py
+++ coda/coda_project/coda_settings/base_settings.py
@@ -66,7 +66,8 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
-    "allauth.socialaccount.providers.facebook"
+    "allauth.socialaccount.providers.facebook",
+    "platform_services",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
