674f0749f3e8a74c8abd4bb15c192abbc957666f
NDOLI Jean Damascene <ndolijeandamascene@gmail.com>
2025-10-31 18:50:52 +0200

    scholarship form

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm, Textarea
 from django.contrib.auth.forms import UserCreationForm
-from accounts.models import CustomerUser 
+from accounts.models import CustomerUser, Scholarship
 from .models import Feedback, Donation_organisation, ContactMessage
 from django.utils.translation import gettext_lazy as _
 
@@ -47,6 +47,45 @@ class MessageForm(forms.ModelForm):
         model = ContactMessage
         fields = ("name", "email", "message")
 
+# scholarship Form
+
+class ScholarshipSearchForm(forms.Form):
+    search_keyword = forms.CharField(
+        required= False,
+        widget =forms.TextInput(attrs={
+            'placeholder':'e.g., STEM, Business, PhP',
+            'class':'w-full p-2 border border-gray-300 rounded-lg focus:ring-brand-blue focus:border-brand-blue'
+        })
+    )
+    filter_level = forms.ChoiceField(
+        required= False,
+        choices=[('All', 'All Level')] + Scholarship.LEVEL_CHOICES,
+        widget =forms.Select(attrs={
+            'class':'w-full p-2 border border-gray-300 rounded-lg focus:ring-brand-blue focus:border-brand-blue bg-white'
+        })
+    )
+    filter_field = forms.ChoiceField(
+        required= False,
+        choices=[('All', 'All Fields')] + Scholarship.FIELDS_CHOICES,
+        widget =forms.Select(attrs={
+            'class':'w-full p-2 border border-gray-300 rounded-lg focus:ring-brand-blue focus:border-brand-blue bg-white'
+        })
+    )
+    filter_location = forms.ChoiceField(
+        required= False,
+        choices=[('All', 'Any Location')] + Scholarship.LOCATION_CHOICES,
+        widget =forms.Select(attrs={
+            'class':'w-full p-2 border border-gray-300 rounded-lg focus:ring-brand-blue focus:border-brand-blue bg-white'
+        })
+    )
+    filter_status = forms.BooleanField(
+        required = False,
+        label = 'show only "Closing Soon"',
+        widget = forms.CheckboxInput(attrs={
+            'class': 'h-4 w-4 text-brand-red border-gray-300 rounded focus:ring-brand-red'
+        })
+    )
+    
 
 
 
