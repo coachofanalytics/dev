1008a216435c317656c161495e535ed54121c7a5
Mwongela <pmzomo@gmail.com>
2025-11-20 13:39:30 +0300

    Fix web dyno crash by disabling Celery import
    - Web dyno was failing because it tried to connect to Redis during Django startup
    - Redis connection fails for web dyno (only worker needs it)
    - Commented out Celery import in config/__init__.py
    - Worker dyno can still import Celery directly when needed

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,5 @@
-# This will make sure the Celery app is always imported when Django starts
-from .celery import app as celery_app
+# Celery import disabled for web dynos - only worker dyno needs it
+# The web dyno was crashing because it tried to connect to Redis on startup
+# from .celery import app as celery_app
 
-__all__ = ('celery_app',)
+# __all__ = ('celery_app',)
