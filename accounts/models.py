50789b502c61e2385a0512893fc61a93d66b1b4e
phinehasmaina <phinehaskiyozeal.maina@calbaptist.edu>
2026-01-12 09:42:36 -0800

    Installed all the dependencies on my branch will check if it ready to run soon

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+#Comment to push at least one change.
 
 class Category(models.Model):
     """
