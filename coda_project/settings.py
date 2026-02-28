7bdc152056a7a955cc8637ad2f08682bd6f3c7f7
Sajghaa <sajghaa@gmail.com>
2026-02-28 16:19:33 +0200

    Add debug_toolbar to requirements

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -63,12 +63,17 @@ INSTALLED_APPS = [
     "django_crontab",
     'memberjoin',
     'communities',
-    #'debug_toolbar',
+    'debug_toolbar',
 
 ]
 
 if DEBUG:
-    INSTALLED_APPS += ['debug_toolbar']
+    try:
+        import debug_toolbar
+        INSTALLED_APPS += ['debug_toolbar']
+        MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    except ImportError:
+        pass # If it's not installed, just don't use it
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
@@ -95,7 +100,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'allauth.account.middleware.AccountMiddleware',
     # 'Middleware.MiddlewareFile.MailMiddleware'
-    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 
 ]
 
