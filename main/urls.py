7a1dbcb0f1a673e229141ee04c54d3fcc85308c3
Serge <sajghaa@gmail.com>
2026-03-21 09:28:24 +0200

    updated urls by adding path of ai_discovery_course

--- main/urls.py
+++ main/urls.py
@@ -75,6 +75,7 @@ urlpatterns = [
     path('education/', views.education_landing, name = 'education_landing'),
     path('education/scholarship/',views.education_landing, name = 'education_scholarship'),
     path('education/training/',views.education_training, name = 'education_training'),
+    path('ai-courses/', views.ai_course_discovery, name='ai_course_discovery'),
     path('education/courses/register/', views.course_register, name='course_register'),
     path('education/request-mentorship/', views.request_mentorship, name='request_mentorship'),
 
