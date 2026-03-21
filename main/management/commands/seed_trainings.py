bff1cd2b63515a1b1a3f36f908c75825a937ecdc
Serge <sajghaa@gmail.com>
2026-03-21 11:29:17 +0200

    added seed_scholarship.py and see_trainings.py

---
+++ main/management/commands/seed_trainings.py
@@ -0,0 +1,28 @@
+# main/management/commands/seed_trainings.py
+
+from django.core.management.base import BaseCommand
+from main.create_training_data import (
+    create_training_courses, create_targeted_courses,
+    create_bulk_training_course, print_course_stats, fix_enrollment_statuses
+)
+
+class Command(BaseCommand):
+    help = 'Seed Training Courses data'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Creating training courses...')
+        create_training_courses(50)
+
+        self.stdout.write('Creating targeted courses...')
+        create_targeted_courses()
+
+        self.stdout.write('Creating bulk training courses...')
+        create_bulk_training_course(60)
+
+        self.stdout.write('Printing course stats...')
+        print_course_stats()
+
+        self.stdout.write('Fixing enrollment statuses...')
+        fix_enrollment_statuses()
+
+        self.stdout.write(self.style.SUCCESS('Training courses seeded successfully'))
