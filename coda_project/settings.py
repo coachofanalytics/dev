7fc19c010fec012895bbcd7dbd2357d42a2d17b4
Davis Crown <davis21crown@gmail.com>
2026-03-21 16:28:31 +0200

    Feat: AJAX to prevent page refresh

--- coda_project/settings.py
+++ coda_project/settings.py
@@ -390,9 +390,9 @@ LOGIN_URL = "accounts:account-login"
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
