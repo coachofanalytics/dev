7c7960ecd77cf4849d137c9f369e374089b02327
Chris Maghas <coachofanalytics@gmail.com>
2026-03-26 14:11:57 +0300

    created views ,template and urls finance_app,in vscod

--- finance_app/views.py
+++ finance_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from finance_app.models import PaymentInformation
+# from finance_app.forms import PaymentInformationForm
 
-# Create your views here.
+
+# =========================
+# LIST VIEW
+# =========================
+def payment_list(request):
+    payments = PaymentInformation.objects.all().order_by("-created_at")
+    return render(request, "finance_app/paymentinformation_list.html", {"payments": payments})
