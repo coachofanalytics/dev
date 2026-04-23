566d4595eb1252acb67c9b3bf319d3158a406053
Serge <sajghaa@gmail.com>
2026-04-23 17:15:07 +0200

    modified our main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.core.models.transactions import transaction
 from app.core.db.database import engine
 from app.v1.transactions import transaction_delete, transaction_post, transaction_update, transaction_get
+from app.v1.auth import auth_post
 
 
 
@@ -12,9 +13,14 @@ app = FastAPI()
 # Create database tables from the models declared in models.py 
 transaction.Base.metadata.create_all(engine)
 
+# import user model after base is defined
+from app.core.models.users import user
+user.Base.metadata.create_all(engine)
+
 
 # Register endpoints
 app.include_router(transaction_get.router)
 app.include_router(transaction_update.router)
 app.include_router(transaction_post.router)
 app.include_router(transaction_delete.router)
+app.include_router(auth_post.router)
