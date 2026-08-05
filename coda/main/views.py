ea7434d88daef1b13c21f741c6fd038a2d725d3a
Luke <info@2009_coda>
2026-08-05 10:32:59 +0300

    update views.py

--- coda/main/views.py
+++ coda/main/views.py
@@ -1884,8 +1884,20 @@ def company_detail(request, company='coda'):
 class SearchHistoryListView(LoginRequiredMixin, ListView):
     model = SearchHistory
     template_name ="main/search_history/list.html"
-    context_object_main ="search_histories"
+    context_object_name="search_histories"
 
+    def get_queryset(self):
+        return SearchHistory.objects.filter(
+            searched_by=self.request.user
+            
+        )
+
+class SearchHistoryDetailView(LoginRequiredMixin,DetailView):
+    model=SearchHistory
+    template_name="main/search_history/detail.html"
+    context_object_name="history"
+
+    
     def get_queryset(self):
         return SearchHistory.objects.filter(
             searched_by=self.request.user
